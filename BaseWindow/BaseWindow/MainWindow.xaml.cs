5a3941ee21bead04c054666c60d9d8032e891967
Pietro Lazzarotto <pietro.lazz@gmail.com>
2017-10-19 15:34:51 +0100

    Showing drives on window loaded

--- BaseWindow/BaseWindow/MainWindow.xaml.cs
+++ BaseWindow/BaseWindow/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace BaseWindow
 {
@@ -7,9 +9,36 @@ namespace BaseWindow
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Contructor
         public MainWindow()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region On Loaded
+
+        /// <summary>
+        /// When the application first opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem {
+                    Header = drive
+                };
+
+                // Add Dummy item to show expand arrow
+                item.Items.Add(null);
+
+                // Add it to the main preview
+                FolderView.Items.Add(item);
+            }
+        }
+
+        #endregion
     }
 }
