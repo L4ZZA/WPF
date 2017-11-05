e28169de31102858614359feb7900baece76f12c
Pietro Lazzarotto <pietro.lazz@gmail.com>
2017-11-05 20:48:25 +0000

    Added base annimations

--- BaseWindow/BaseWindow/Pages/BasePage.cs
+++ BaseWindow/BaseWindow/Pages/BasePage.cs
@@ -1,12 +1,89 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace BaseWindow
 {
     /// <summary>
-    /// 
+    /// A base page for all pages to gain base functionalities
     /// </summary>
     public class BasePage : Page
     {
+        #region Public Properties
         
+        /// <summary>
+        /// The animation that plays when the page is first Loaded
+        /// </summary>
+        public PageAnimation PageLoadAnimation { get; set; } = PageAnimation.SlideAndFadeFromRight;
+
+        /// <summary>
+        /// The animation that plays when the page is first Unloaded
+        /// </summary>
+        public PageAnimation PageUnloadAnimation { get; set; } = PageAnimation.SlideAndFadeOutToLeft;
+
+        /// <summary>
+        /// Time the slide anymation takes to complete
+        /// </summary>
+        public float SlideSeconds { get; set; } = 0.8f;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BasePage()
+        {
+            // Don't bother animating in design time
+            if (DesignerProperties.GetIsInDesignMode(this))
+                return;
+
+            // If we are animating in, hide to begin with
+            if (PageLoadAnimation != PageAnimation.None)
+                Visibility = Visibility.Collapsed;
+
+            // Listen out for the page loading
+            Loaded += BasePage_LoadedAsync;
+        }
+
+        #endregion
+
+        #region Animation Load/Unload
+
+        /// <summary>
+        /// Perform any required animation once the page is loaded
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void BasePage_LoadedAsync(object sender, System.Windows.RoutedEventArgs e)
+        {
+            await AnimateIn();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task AnimateIn()
+        {
+            if (this.PageLoadAnimation == PageAnimation.None)
+                return;
+
+            switch (PageLoadAnimation)
+            {
+                case PageAnimation.SlideAndFadeFromRight:
+
+                    // Start animation
+                    await this.SlideAndFadeInFromRightAsync(SlideSeconds);
+
+                    break;
+            }
+        }
+
+        #endregion
     }
 }
