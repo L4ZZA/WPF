fe55d1ccf1add0256f9499ae89f51e2428b94e63
Pietro Lazzarotto <pietro.lazz@gmail.com>
2017-11-05 10:10:49 +0000

    Changed TextBox for PasswordBox
    And added the 2 new AttachedProperties required for the correct logic as well as the BoolToVisibility converter

---
+++ BaseWindow/BaseWindow/AttachedProperties/PasswordBoxAttachedProperties.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BaseWindow
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MonitorPasswordProperty : BaseAttachedProperty<MonitorPasswordProperty, bool>
+    {
+        public override void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            // Get the caller
+            var passwordBox = sender as PasswordBox;
+
+            // Make sure it is a PasswordBox
+            if (passwordBox == null)
+                return;
+
+            // Remove any previous event
+            passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+
+            // If caller set MonitorPassword to True
+            if ((bool) e.NewValue)
+            {
+                // Set default value
+                HasTextProperty.SetValue(passwordBox);
+
+                // Start listening for password changes
+                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            }
+        }
+
+        /// <summary>
+        /// Fired when the pasword box password value changes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            // Set the attached HasText value
+            HasTextProperty.SetValue((PasswordBox)sender);
+        }
+    }
+
+    /// <summary>
+    /// The HasText attached property for a <see cref="PasswordBox"/>
+    /// </summary>
+    public class HasTextProperty : BaseAttachedProperty<HasTextProperty, bool>
+    {
+        public static void SetValue(DependencyObject sender)
+        {
+            SetValue(sender, ((PasswordBox)sender).SecurePassword.Length > 0);
+        }
+    }
+
+
+}
