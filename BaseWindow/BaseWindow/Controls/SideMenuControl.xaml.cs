c240d0774154ae9bf2c6112c3be9249d4dd78fbf
Pietro Lazzarotto <pietro.lazz@gmail.com>
2018-03-18 21:29:33 +0000

    Moved SideMenu to user control

---
+++ BaseWindow/BaseWindow/Controls/SideMenuControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace BaseWindow
+{
+    /// <summary>
+    /// Interaction logic for SideMenuControl.xaml
+    /// </summary>
+    public partial class SideMenuControl : UserControl
+    {
+        public SideMenuControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
