4f1ac87e25b500664d0fb74f1ec32515dd851595
pietro <pietro@green-hippo.com>
2018-03-23 18:50:03 +0000

    Implemented viewModel and added design time model

---
+++ BaseWindow/BaseWindow/ViewModels/Chat/ChatListItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace BaseWindow.ViewModels.Chat
+{
+    /// <summary>
+    /// A view model for each chat item in the overview chat list
+    /// </summary>
+    public class ChatListItemViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// The display name of this chat list
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The latest message from this chat
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// The initials to show for the profile picture background
+        /// </summary>
+        public string Initials { get; set; }
+
+        /// <summary>
+        /// The RGB values (in hex) for the background color of the profile picture
+        /// For example FF00FF forr Red and Blue mixed
+        /// </summary>
+        public string ProfilePictureRGB { get; set; }
+    }
+}
---
+++ BaseWindow/BaseWindow/ViewModels/Chat/Design/ChatListItemDesignModel.cs
@@ -0,0 +1,33 @@
+﻿namespace BaseWindow.ViewModels.Chat.Design
+{
+    /// <summary>
+    /// the design-time data for a <see cref="ChatListItemViewModel"/>
+    /// </summary>
+    public class ChatListItemDesignModel : ChatListItemViewModel
+    {
+
+        #region Singletone
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static ChatListItemDesignModel Instance { get; } = new ChatListItemDesignModel();
+
+        #endregion
+
+        #region Contructor
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ChatListItemDesignModel()
+        {
+            Initials = "LM";
+            Name = "Luke";
+            Message = "This new chat app is awesome! I bet it will be fasst too";
+            ProfilePictureRGB = "3099c5";
+        }
+
+        #endregion
+    }
+}
